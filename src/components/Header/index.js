0ea64e69731319f71e263d30b5c0fccef6abb7b8
tbow5257 <133tjb@gmail.com>
2018-02-13 19:25:36 -0800

    feat: Create header, add Styled-Components && React-Motion

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,84 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from "./logo.png"
+import styled from "styled-components"
+
+const HeaderLogoTitle = styled.div`
+display: flex;
+justify-content: flex-start;
+align-items: center;
+h1, h3 { 
+  margin-left: 5px;
+}
+h1 {
+  margin-bottom:0;
+   }
+   
+h3 {
+  color: #59be6b;
+}
+`;
+
+const HeaderTitle = styled.div`
+  display: flex;
+  align-items: left;
+  flex-flow: column;
+  align-self: center;
+`
+
+const HeaderAddress = styled.div`
+margin: 10px;
+display: flex;
+justify-content: flex-end;
+flex-direction: column;
+h4 {
+  margin-bottom: 0.1rem;
+}
+`;
+
+
+
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
+        style={{
+            background: 'white',
+            marginBottom: '1.45rem',
+        }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
+        <div
+            style={{
+                display: 'flex',
+                'justify-content': 'space-between',
+                'align-items': 'center',
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '1.45rem 1.0875rem',
+            }}
         >
-          Gatsby
-        </Link>
-      </h1>
+            <HeaderLogoTitle>
+                <img src={logo} alt="Logo"/>
+                <Link
+                    to="/"
+                    style={{
+                        color: '#295c33',
+                        textDecoration: 'none',
+                    }}
+                >
+                    <HeaderTitle>
+                        <h1>Bowen Dental Care</h1>
+                        <h1>General Dentistry</h1>
+                        <h3>Healthy Mouth, Healthy Body</h3>
+                    </HeaderTitle>
+                </Link>
+            </HeaderLogoTitle>
+            <HeaderAddress>
+                <h4>760-781-3360</h4>
+                <h4>202 South Juniper St.</h4>
+                <h4>Escondido, CA 92025</h4>
+            </HeaderAddress>
+        </div>
     </div>
-  </div>
-)
+);
 
 export default Header
